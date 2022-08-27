a5a6a52f6d84c79a9a48b5b67ef098a724d9754f
yabdulra <yusufjauro@gmail.com>
2022-08-26 21:28:47 -0600

    feat: updated to plot simulated obstacles

--- trajectory_tracking/scripts/plotter.py
+++ trajectory_tracking/scripts/plotter.py
@@ -4,13 +4,15 @@
 import rospy
 from nav_msgs.msg import Odometry
 from trajectory_tracking.msg import Traj
-from geometry_msgs.msg import Pose
+
+import rospkg
 
 # import Matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 import math
+import random
 
 # global variables decalration 
 global traj, robot_x, robot_y
@@ -21,6 +23,20 @@ xd = []
 yd = [] 
 xrobot = []
 yrobot = []
+colors = [
+    (0.078, 0.804, 0.784, 1), (0.000, 0.412, 0.573, 1), (0.671, 0.718, 0.718, 1),
+    (0.463, 0.365, 0.412, 1), (1.000, 0.580, 0.439, 1), (0.424, 0.478, 0.537, 1),
+    (0.686, 0.255, 0.329, 1), (0.945, 0.353, 0.133, 1), (0.431, 0.188, 0.294, 1),
+    (0.635, 0.871, 0.817, 1)
+]
+
+def obstacles(path):
+    global obsts
+    with open(path + '/src/obstacles.txt') as f:
+        obsts = f.readlines()
+    obsts = [obst.strip() for obst in obsts]
+    for i in range(len(obsts)):
+        obsts[i] = [float(x) for x in obsts[i].split()]
 
 # callback function for odom data
 def odom_callbk(msg):
@@ -46,7 +62,9 @@ def animate1(i):
     xrobot.append(float(robot_x))
     yrobot.append(float(robot_y))
     ax1.plot(xrobot, yrobot, 'k', linestyle='dashdot', label='robot trajectory', lw=1)
-
+    
+    for obst in obsts:
+        ax1.add_artist( plt.Circle((obst[0], obst[1]), obst[2], color=colors[random.randint(0, 9)], alpha=0.9))
     
     ax1.grid(True)
     ax1.legend()
@@ -62,6 +80,10 @@ if __name__ == '__main__':
 
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(111)
+
+    rospack = rospkg.RosPack()
+    path = rospack.get_path('trajectory_tracking')
+    obstacles(path)
 
     # uncomment to set axis limits
     # ax1.set_xlim([-17, 17])
